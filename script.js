b088880a109378496c546eec1c401381ea9c844a
Amritanshu <amritanshu656@gmail.com>
2026-09-06 21:13:24 +0530

    Add

---
+++ script.js
@@ -0,0 +1,27 @@
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+displayTasks();
+function addTask() {
+    let input = document.getElementById("taskInput");
+    let task = input.value;
+    if(task === "") return;
+    tasks.push(task);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    input.value = "";
+    displayTasks();
+}
+function displayTasks(){
+    let list = document.getElementById("taskList");
+    list.innerHTML = "";
+    tasks.forEach((task,index)=>{
+        let li = document.createElement("li");
+        li.innerHTML = `
+        ${task}
+        <button class="delete" onclick="deleteTask(${index})">X</button>`;
+        list.appendChild(li);
+    });
+}
+function deleteTask(index){
+    tasks.splice(index,1);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    displayTasks();
+}
